512fd8ff599c7e4a664902d69d5a7bb426ffae39
Toshiy Do <toshiydo@gmail.com>
2021-09-24 17:38:05 +0500

    Добавил переходы между страницами

--- locators.py
+++ locators.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "input[id='id_login-username']")
-    REGISTER_FORM = (By.CSS_SELECTOR, "input[id='id_registration-email']")
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     
--- login_page.py
+++ login_page.py
@@ -1,5 +1,4 @@
 from .base_page import BasePage
-from selenium import webdriver
 from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
@@ -10,10 +9,15 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert 'login' in self.driver.current_url, 'LOGIN not in url'
+        assert "login" in self.browser.current_url, "This is not URL for login"
+        assert True
 
     def should_be_login_form(self):
-        assert self.is_element_present(LoginPageLocators.LOGIN_FORM), "LOGIN not presented"
-        
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login dorm is not present"
+        assert True
+
     def should_be_register_form(self):
-        assert self.is_element_present(LoginPageLocators.REGISTER_FORM), "REGISTRATION is not presented"
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not present"
+        assert True
