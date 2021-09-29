eaeae6429496659c8771e97aeac1b02f0ab54cf9
Toshiy Do <toshiydo@gmail.com>
2021-09-29 16:16:01 +0500

    добавили класс для локаторов и их сами

--- locators.py
+++ locators.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 
-class MainPageLocators():
+class ProductPageLocators:
+    BUTTON = (By.CSS_SELECTOR, ".btn.btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main p.price_color")
+    NAME_TRASH = (By.CSS_SELECTOR, ".alertinner strong")
+    PRICE_TRASH = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "[id='messages'] .alertinner")
+
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-class LoginPageLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     
