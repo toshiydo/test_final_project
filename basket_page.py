b25e9308b0701d72425b2ee952a3fa888bdac8c7
toshiydo <54236992+toshiydo@users.noreply.github.com>
2021-10-04 18:59:09 +0500

    Delete basket_page.py

--- basket_page.py
+++
@@ -1,9 +0,0 @@
-from .locators import BasketPageLocators
-from .base_page import BasePage
-from selenium import webdriver
-class BasketPage(BasePage):
-    def no_product_in_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.NO_PRODUCT_IN_BASKET), "product in basket"
-
-    def have_text_in_basket(self):
-        assert 'Your basket is empty.' in self.browser.find_element(*BasketPageLocators.BASKET_TEXT).text, "no TEXT"
