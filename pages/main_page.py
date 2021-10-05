13ae38704222bd9ea92ba93285bfa313aaea9069
Toshiy Do <toshiydo@gmail.com>
2021-10-05 17:41:54 +0500

    изменил поиск элемента в двух функциях

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
+from .locators import MainPageLocators
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "login_link")
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK)
