302187c0d42276aa0f47c2953d4496585f7e46b1
Toshiy Do <toshiydo@gmail.com>
2021-10-05 18:04:14 +0500

    убрали лишний код

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,13 @@
+from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+      
 
     def should_be_login_link(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK)
