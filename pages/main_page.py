1e42c1ecef78fb94cc95bb1f5de6160da21f2623
Toshiy Do <toshiydo@gmail.com>
2021-10-06 17:25:14 +0500

    сделал затычку

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,6 @@
-from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-      
-
-    def should_be_login_link(self):
-        self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
