d82117fffcb2876cab205b6651efe5e4843e843f
Toshiy Do <toshiydo@gmail.com>
2021-10-07 14:28:21 +0500

    добавил новый способ для регистрации пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,3 @@
-from typing import FrozenSet
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
@@ -19,3 +18,14 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         assert True
+
+    def register_new_user(self, email, password):
+        self.should_be_register_form()
+        self.input_text(*LoginPageLocators.REGISTER_EMAIL_ADDRESS, email)
+        self.input_text(*LoginPageLocators.REGISTER_PASSWORD, password)
+        self.input_text(*LoginPageLocators.REGISTER_PASSWORD_CONFIRM, password)
+        self.click_button(*LoginPageLocators.REGISTER_BUTTON)
+
+
+        
+
